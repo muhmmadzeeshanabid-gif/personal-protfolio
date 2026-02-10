ca1d28c7642acc0c15a88c7da553564da16aada6
itx_affii <itx.affii@gmail.com>
2026-02-10 10:59:55 +0500

    Portfolio Fixes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/My-Protfolio/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
